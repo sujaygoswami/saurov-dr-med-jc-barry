b15e59cb93e7060d425b52a5d13b952c571a45d3
Sujay Goswami <sujaygoswami@Sujays-Mac-mini.local>
2024-08-02 12:18:35 +0530

    work update dated 02.08.2024 1st push

---
+++ html-master/JavaScript/init/main.js
@@ -0,0 +1,203 @@
+jQuery(document).ready(function(){
+
+
+
+
+
+
+
+
+
+function isAppleDevice(){
+return (
+(navigator.userAgent.toLowerCase().indexOf("ipad") > -1) ||
+(navigator.userAgent.toLowerCase().indexOf("iphone") > -1) ||
+(navigator.userAgent.toLowerCase().indexOf("ipod") > -1)
+);
+}
+var isAndroid = navigator.userAgent.toLowerCase().indexOf("android");
+var appStoreURL = "";
+if(isAndroid > -1){
+jQuery('body').addClass('android-device');
+jQuery('html').addClass('android-device');
+}
+else if( isAppleDevice() ){
+jQuery('body').addClass('ios-device');
+jQuery('html').addClass('ios-device');
+
+  // jQuery( window ).on( "orientationchange", function( event ) {
+  //   location.reload();
+  // });
+
+
+}
+
+
+
+
+
+//Check Mobile Devices
+var checkMobile = function(){
+
+    //Check Device
+    var isTouch = ('ontouchstart' in document.documentElement);
+
+    //Check Device //All Touch Devices
+    if ( isTouch ) {
+
+        jQuery('body').addClass('touch');
+
+        
+
+    }
+    else {
+
+        jQuery('body').addClass('no-touch');
+
+    };
+
+};
+
+//Execute Check
+checkMobile();
+
+
+
+
+
+
+
+
+
+
+
+// object notation
+var site = {}
+
+
+
+site.FIRSTLOOK = function() {
+
+    
+
+
+    
+
+
+};
+
+
+
+
+
+
+jQuery(window).resize(function(){
+
+
+});
+
+
+
+jQuery( window ).on( "orientationchange", function( event ) {
+
+   
+          
+});
+
+
+
+
+
+
+
+
+// set back
+
+jQuery('.set-back').each(function(){
+
+ var SETBACK = jQuery(this).find('img').attr('src');
+  jQuery(this).css('background-image', 'url(' + SETBACK + ')');
+
+});
+
+
+
+
+
+
+
+
+
+
+jQuery('.touch .has-submenu > a').addClass('dual-click');
+   
+   jQuery(document).on( "click", ".dual-click", function(event) {
+        
+        jQuery(".dual-click").not(this).removeClass("clicked");
+        jQuery(this).toggleClass("clicked");
+        if (jQuery(this).hasClass("clicked")) {
+            event.preventDefault();
+        }
+    });
+
+
+// go top
+ jQuery('.go-top').click(function(){
+   jQuery('html, body').animate({scrollTop: 0}, 400);
+  
+});      
+
+
+});
+
+
+
+
+
+
+
+
+// load
+jQuery(window).load(function(){
+
+
+
+
+// match height
+  jQuery('.equal-row').each(function() {
+       jQuery(this).children('.equal-block').matchHeight();
+  });
+
+  jQuery('.element-row').each(function() {
+       jQuery(this).children('.equal-block').matchHeight();
+  });
+
+
+
+
+
+
+
+var siteLoad = {}
+
+
+
+
+siteLoad.CAPTIONSLIDERHEIGHT = function() {
+
+  
+    
+
+};   
+
+
+
+
+jQuery(window).resize(function(){
+
+
+});
+
+
+});
+
